af21871d26409925d745f2fa0f52abfa4dece335
Ese Kelvin Uvbiekpahor <eseboy24@gmail.com>
2023-05-24 11:42:36 +0100

    Display error

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+echo "dd";
+exit;
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
