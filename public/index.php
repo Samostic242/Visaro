aac86882b8effe020c975d59670412ca356853b7
Ese Kelvin Uvbiekpahor <eseboy24@gmail.com>
2023-05-24 13:28:16 +0100

    Update On Index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
-echo "dd";
-exit;
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
