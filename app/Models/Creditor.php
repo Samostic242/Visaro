691432ea3ecf334335b3f7844ad791feb7d0342c
agumaxwellforcode <agumaxwell9@gmail.com>
2023-12-10 19:10:18 +0100

    Added istitution model

---
+++ app/Models/Creditor.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Creditor extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $fillable = [
+        'name',
+        'acronym',
+        'official_name',
+        'website',
+        'logo',
+        'code',
+        'category',
+        'country_id',
+        'email',
+        'phone_code',
+        'phone',
+        'meta',
+        'active',
+    ];
+    protected $casts = [
+        'active' => 'boolean',
+        'meta' => 'array',
+    ];
+}
